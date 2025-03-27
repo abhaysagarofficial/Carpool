d8b4198309af01a6436624c7978661329e785330
abhaysagarofficial <abhaysagarofficial@gmail.com>
2025-03-28 03:13:49 +0530

    Adding search rides section

--- app/search_rides/page.tsx
+++ app/search_rides/page.tsx
@@ -3,8 +3,19 @@
 
 function Search_Rides() {
     return (
-        <div>
-            <h1>This search rides</h1>
+        <div className="flex justify-center items-center h-screen bg-gray-200 text-white" >
+            <div className="bg-gray-800 h-[100%] w-[50%]">
+                <form action="" className="flex flex-col justify-center items-center h-screen">
+                    <label htmlFor="from" className="m-8 text-xl text-bold">From Where : </label>
+                    <input type="text" className="border-2 border-gray-700 m-8" />
+                    <label htmlFor="from" className="m-8 text-xl text-bold">To Where : </label>
+                    <input type="text" className="border-2 border-gray-700 m-8"/>
+                    <label htmlFor="from" className="m-8 text-xl text-bold">Date : </label>
+                    <input type="date" className="border-2 border-gray-700 m-8" />
+                    <button className="border-2 border-gray-700 m-8">Search</button>
+                </form>
+            </div>
+            <div className="bg-pink-800 h-[100%] w-[50%]">this is div 2</div>
         </div>
     );
 }
